dbac879e79c83c824be5aa0ce135618e99748111
ashish ghosh <ghoshashish43@gmail.com>
2020-08-17 10:10:51 +0530

    Updates

---
+++ precache-manifest.ebe1f2af14bbb1e6db5f852ed9b496ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5825e6cf90e818e83777bfb44bda8d",
+    "url": "/facebook-messanger/index.html"
+  },
+  {
+    "revision": "72e238c3f8930e9f6d87",
+    "url": "/facebook-messanger/static/css/main.80ed8696.chunk.css"
+  },
+  {
+    "revision": "5637c43a10b12428e7e5",
+    "url": "/facebook-messanger/static/js/2.6f2a5126.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebook-messanger/static/js/2.6f2a5126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e238c3f8930e9f6d87",
+    "url": "/facebook-messanger/static/js/main.8dd92250.chunk.js"
+  },
+  {
+    "revision": "3475581c2ecdb4bf14b0",
+    "url": "/facebook-messanger/static/js/runtime-main.f8a964d6.js"
+  }
+]);
